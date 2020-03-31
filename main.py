c77b1880248b96547c16c6a9264a894cbaf5a5fc
KilMer56 <mer.killian56@gmail.com>
2020-03-31 10:58:26 +0200

    Add Docker configuration

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ from app_bridge import Bridge
 import schedule
 import time
 
-print("[*] Running script every hour")
+print("[*] Running script every day at midnight")
 
 bridge = Bridge()
 bridge.initCredentials()
-bridge.downloadYoutubePlaylist()
 
-# def fetch():
-#     bridge.fetchYoutubePlaylist()
+def fetch():
+    bridge.fetchYoutubePlaylist()
 
-# try:
-#     fetch()
-#     schedule.every(1).hour.do(fetch)
+try:
+    fetch()
+    schedule.every().day.at("00:00").do(fetch)
 
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
-# except Exception as e:
-#     print(e)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+except Exception as e:
+    print(e)
